a1ef59dfd655b98b7407dcb6df832a71ebeadb1c
Azis202017 <azisa6980@gmail.com>
2023-07-13 22:40:29 +0700

    [Theme] color add to accent color

--- lib/app/shared/theme/color.dart
+++ lib/app/shared/theme/color.dart
@@ -14,9 +14,21 @@ const Color primary6 = Color(0xff1D5532);
 const Color primary7 = Color(0xff164026);
 const Color primary8 = Color(0xff0E2A19);
 const Color primary85 = Color(0xff0B2013);
-
 const Color primary9 = Color(0xff07150D);
 const Color primary95 = Color(0xff040B06);
+
+const Color accent0 = Color(0xffB6E2AB);
+const Color accent05 = Color(0xffA7D39C);
+const Color accent1 = Color(0xff99C48E);
+const Color accent15 = Color(0xff8AB580);
+const Color accent2 = Color(0xff7CA671);
+const Color accent3 = Color(0xff5E8854);
+const Color accent5 = Color(0xff416A37);
+const Color accent6 = Color(0xff1D3D15);
+const Color accent7 = Color(0xff162E10);
+const Color accent8 = Color(0xff0E1E0A);
+const Color accent85 = Color(0xff0B1708);
+const Color accent9 = Color(0xff070F05);
 
 const Color placeHolder = Color(0xffC8C8C8);
 const Color black = Colors.black;
