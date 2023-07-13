247628f22956cfaeb09fbfd56e0163b2b5f68720
Azis202017 <azisa6980@gmail.com>
2023-07-13 22:41:28 +0700

    [Theme] added white to theme color

--- lib/app/shared/theme/color.dart
+++ lib/app/shared/theme/color.dart
@@ -35,3 +35,4 @@ const Color black = Colors.black;
 const Color font = Color(0xff1E1E1E);
 const Color secondaryColor2 = Color(0xffF4FBF7);
 const Color fontAbu = Color(0xff686868);
+const Color whiteColor = Colors.white;
