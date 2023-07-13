b684ea29b52ded87f22b41119d1e5377ff5494fa
Azis202017 <azisa6980@gmail.com>
2023-07-13 22:36:25 +0700

    [Theme]add schema color for primary

---
+++ lib/app/shared/theme/color.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Color(0xff2C6F05);
+const Color secondaryColor = Color(0xff244C1A);
+const Color accentColor = Color(0xffF4FBF7);
+const Color primary0 = Color(0xffC9EDD7);
+const Color primary05 = Color(0xffB8E0C8);
+const Color primary1 = Color(0xffA8D3B9);
+const Color primary15 = Color(0xff97C6A9);
+const Color primary2 = Color(0xff87B99A);
+const Color primary3 = Color(0xff669E7C);
+const Color primary4 = Color(0xff45845D);
+const Color primary6 = Color(0xff1D5532);
+const Color primary7 = Color(0xff164026);
+const Color primary8 = Color(0xff0E2A19);
+const Color primary85 = Color(0xff0B2013);
+
+const Color primary9 = Color(0xff07150D);
+const Color primary95 = Color(0xff040B06);
+
+const Color placeHolder = Color(0xffC8C8C8);
+const Color black = Colors.black;
+const Color font = Color(0xff1E1E1E);
+const Color secondaryColor2 = Color(0xffF4FBF7);
+const Color fontAbu = Color(0xff686868);
