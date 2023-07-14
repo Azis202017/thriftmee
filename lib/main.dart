001fb8f7c29c173d4b3b7e6692708348a1e3fde0
Azis202017 <azisa6980@gmail.com>
2023-07-14 11:53:51 +0700

    [Main] added main

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'app/routes/app_pages.dart';
 import 'app/shared/theme/theme.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     GetMaterialApp(
       title: "ThriftMee",
