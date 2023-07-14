f652776e42001cd4e74cd346b5c5a056a9726364
Azis202017 <azisa6980@gmail.com>
2023-07-14 11:54:13 +0700

    [Login] added login view

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,9 +2,10 @@ import 'package:get/get.dart';
 
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/login/bindings/login_binding.dart';
+import '../modules/login/views/login_view.dart';
 import '../modules/register/bindings/register_binding.dart';
 import '../modules/register/views/register_view.dart';
-
 
 part 'app_routes.dart';
 
@@ -23,6 +24,11 @@ class AppPages {
       name: _Paths.REGISTER,
       page: () => const RegisterView(),
       binding: RegisterBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
     ),
   ];
 }
