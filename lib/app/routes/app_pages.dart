8602cc22279406c475285cf2c83f880134cffe18
Azis202017 <azisa6980@gmail.com>
2023-08-03 10:39:17 +0700

    Add pages

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,15 +1,29 @@
 import 'package:get/get.dart';
 
+import '../modules/cart/bindings/cart_binding.dart';
+import '../modules/cart/views/cart_view.dart';
+import '../modules/detail_product/bindings/detail_product_binding.dart';
+import '../modules/detail_product/views/detail_product_view.dart';
 import '../modules/forgot-password/bindings/forgot_password_binding.dart';
 import '../modules/forgot-password/views/forgot_password_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
+import '../modules/notification/bindings/notification_binding.dart';
+import '../modules/notification/views/notification_view.dart';
 import '../modules/onboarding/bindings/onboarding_binding.dart';
 import '../modules/onboarding/views/onboarding_view.dart';
 import '../modules/register/bindings/register_binding.dart';
 import '../modules/register/views/register_view.dart';
+import '../modules/settings/bindings/settings_binding.dart';
+import '../modules/settings/views/settings_view.dart';
+import '../modules/settings_other/bindings/settings_other_binding.dart';
+import '../modules/settings_other/views/settings_other_view.dart';
+import '../modules/settings_pesanan/bindings/settings_pesanan_binding.dart';
+import '../modules/settings_pesanan/views/settings_pesanan_view.dart';
+import '../modules/try_on/bindings/try_on_binding.dart';
+import '../modules/try_on/views/try_on_view.dart';
 import '../modules/verifikasi_password/bindings/verifikasi_password_binding.dart';
 import '../modules/verifikasi_password/views/verifikasi_password_view.dart';
 
@@ -18,7 +32,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.ONBOARDING;
+  static const INITIAL = Routes.DETAIL_PRODUCT;
 
   static final routes = [
     GetPage(
@@ -50,6 +64,41 @@ class AppPages {
       name: _Paths.VERIFIKASI_PASSWORD,
       page: () => const VerifikasiPasswordView(),
       binding: VerifikasiPasswordBinding(),
+    ),
+    GetPage(
+      name: _Paths.NOTIFICATION,
+      page: () => const NotificationView(),
+      binding: NotificationBinding(),
+    ),
+    GetPage(
+      name: _Paths.SETTINGS,
+      page: () => const SettingsView(),
+      binding: SettingsBinding(),
+    ),
+    GetPage(
+      name: _Paths.SETTINGS_PESANAN,
+      page: () => const SettingsPesananView(),
+      binding: SettingsPesananBinding(),
+    ),
+    GetPage(
+      name: _Paths.SETTINGS_OTHER,
+      page: () => const SettingsOtherView(),
+      binding: SettingsOtherBinding(),
+    ),
+    GetPage(
+      name: _Paths.CART,
+      page: () => const CartView(),
+      binding: CartBinding(),
+    ),
+    GetPage(
+      name: _Paths.DETAIL_PRODUCT,
+      page: () => const DetailProductView(),
+      binding: DetailProductBinding(),
+    ),
+    GetPage(
+      name: _Paths.TRY_ON,
+      page: () => const TryOnView(),
+      binding: TryOnBinding(),
     ),
   ];
 }
