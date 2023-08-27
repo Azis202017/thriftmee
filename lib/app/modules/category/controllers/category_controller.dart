5f56a501ac74ac9a7dab442de998dd255697c965
Azis202017 <azisa6980@gmail.com>
2023-08-27 08:01:34 +0700

    Slicing category

---
+++ lib/app/modules/category/controllers/category_controller.dart
@@ -0,0 +1,6 @@
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:get/get.dart';
+
+class CategoryController extends GetxController {
+   CarouselController buttonCarouselController = CarouselController();
+}
