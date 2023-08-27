9835207e007b281fc22bfb50c8ec9ff2c749cf62
Azis202017 <azisa6980@gmail.com>
2023-08-27 08:39:39 +0700

    create a new navigation bottom bar

--- lib/app/modules/navigation_bottom/controllers/navigation_bottom_controller.dart
+++ lib/app/modules/navigation_bottom/controllers/navigation_bottom_controller.dart
@@ -1,14 +1,19 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:thriftmee/app/modules/category/views/category_view.dart';
 import 'package:thriftmee/app/modules/feed/views/feed_view.dart';
 import 'package:thriftmee/app/modules/home/views/home_view.dart';
 
+import '../../../constant/image_collection.dart';
 import '../../profile/views/profile_view.dart';
 import '../../shop/views/shop_view.dart';
 
 class NavigationBottomController extends GetxController {
   int selectedIndex = 0;
+  PersistentTabController navController =
+      PersistentTabController(initialIndex: 0);
+
   List<Widget> page = [
     const HomeView(),
     const CategoryView(),
@@ -19,5 +24,35 @@ class NavigationBottomController extends GetxController {
   void onItemTapped(int index) {
     selectedIndex = index;
     update();
+  }
+
+  List<PersistentBottomNavBarItem> navBarsItems() {
+    return [
+     
+      PersistentBottomNavBarItem(
+        icon: const Icon(CupertinoIcons.settings),
+        title: "Settings",
+        activeColorPrimary: CupertinoColors.activeBlue,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(CupertinoIcons.settings),
+        title: "Settings",
+        activeColorPrimary: CupertinoColors.activeBlue,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(CupertinoIcons.settings),
+        title: "Settings",
+        activeColorPrimary: CupertinoColors.activeBlue,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(CupertinoIcons.settings),
+        title: "Settings",
+        activeColorPrimary: CupertinoColors.activeBlue,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+    ];
   }
 }
--- lib/app/modules/navigation_bottom/views/navigation_bottom_view.dart
+++ lib/app/modules/navigation_bottom/views/navigation_bottom_view.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:thriftmee/app/constant/image_collection.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:thriftmee/app/modules/category/controllers/category_controller.dart';
 import 'package:thriftmee/app/modules/home/controllers/home_controller.dart';
+import 'package:thriftmee/app/shared/theme/color.dart';
 import 'package:thriftmee/app/shared/theme/font.dart';
 
+import '../../../constant/image_collection.dart';
 import '../controllers/navigation_bottom_controller.dart';
 
 class NavigationBottomView extends GetView<NavigationBottomController> {
@@ -15,130 +17,250 @@ class NavigationBottomView extends GetView<NavigationBottomController> {
     Get.put(HomeController(), permanent: true);
     Get.put(CategoryController(), permanent: true);
 
+    return GetBuilder<NavigationBottomController>(builder: (_) {
+      // return Scaffold(
+      //   body: controller.page.elementAt(controller.selectedIndex),
+      //   bottomNavigationBar:
 
-    return GetBuilder<NavigationBottomController>(builder: (context) {
-      return Scaffold(
-        body: controller.page.elementAt(controller.selectedIndex),
-        bottomNavigationBar: Padding(
-          padding: const EdgeInsets.only(
-            top: 12,
-            left: 16,
-            right: 16,
-            bottom: 15,
-          ),
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: Colors.white,
-            selectedLabelStyle: h5Medium,
-            iconSize: 24,
-            unselectedLabelStyle: h5Medium,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Padding(
-                  padding: EdgeInsets.only(bottom: 8.0),
-                  child: ImageIcon(
-                    NetworkImage(
-                      homeIconInactive,
-                    ),
-                  ),
-                ),
-                activeIcon: Padding(
-                  padding: EdgeInsets.only(bottom: 8.0),
-                  child: ImageIcon(
-                    NetworkImage(
-                      homeIcon,
-                    ),
-                  ),
-                ),
-                label: 'Beranda',
-                tooltip: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Padding(
-                  padding: EdgeInsets.only(bottom: 8.0),
-                  child: ImageIcon(
-                    NetworkImage(
-                      categoryIcon,
-                    ),
-                  ),
-                ),
-                activeIcon: Padding(
-                  padding: EdgeInsets.only(bottom: 8.0),
-                  child: ImageIcon(
-                    NetworkImage(
-                      categoryIconActive,
-                    ),
-                  ),
-                ),
-                label: 'Kategori',
-                tooltip: 'Kategori',
-              ),
-              BottomNavigationBarItem(
-                icon: Padding(
-                  padding: EdgeInsets.only(bottom: 8.0),
-                  child: ImageIcon(
-                    NetworkImage(
-                      shop,
-                    ),
-                  ),
-                ),
-                activeIcon: Padding(
-                  padding: EdgeInsets.only(bottom: 8.0),
-                  child: ImageIcon(
-                    NetworkImage(
-                      shopActive,
-                    ),
-                  ),
-                ),
-                label: 'Toko',
-                tooltip: 'Toko',
-              ),
-              BottomNavigationBarItem(
-                icon: Padding(
-                  padding: EdgeInsets.only(bottom: 8.0),
-                  child: ImageIcon(
-                    NetworkImage(
-                      feed,
-                    ),
-                  ),
-                ),
-                activeIcon: Padding(
-                  padding: EdgeInsets.only(bottom: 8.0),
-                  child: ImageIcon(
-                    NetworkImage(
-                      feedActive,
-                    ),
-                  ),
-                ),
-                label: 'Feed',
-                tooltip: 'Feed',
-              ),
-              BottomNavigationBarItem(
-                icon: Padding(
-                  padding: EdgeInsets.only(bottom: 8.0),
-                  child: ImageIcon(
-                    NetworkImage(
-                      profile,
-                    ),
-                  ),
-                ),
-                activeIcon: Padding(
-                  padding: EdgeInsets.only(bottom: 8.0),
-                  child: ImageIcon(
-                    NetworkImage(
-                      profileActive,
-                    ),
-                  ),
-                ),
-                label: 'Profile',
-                tooltip: 'Profile',
-              ),
-            ],
-            currentIndex: controller.selectedIndex,
-            elevation: 0,
-            onTap: controller.onItemTapped,
-          ),
+      //   Padding(
+      //     padding: const EdgeInsets.only(
+      //       top: 12,
+      //       left: 16,
+      //       right: 16,
+      //       bottom: 15,
+      //     ),
+      //     child: BottomNavigationBar(
+      //       type: BottomNavigationBarType.fixed,
+      //       backgroundColor: Colors.white,
+      //       selectedLabelStyle: h5Medium,
+      //       iconSize: 24,
+      //       unselectedLabelStyle: h5Medium,
+      //       items: const [
+      //         BottomNavigationBarItem(
+      //           icon: Padding(
+      //             padding: EdgeInsets.only(bottom: 8.0),
+      //             child: ImageIcon(
+      //               NetworkImage(
+      //                 homeIconInactive,
+      //               ),
+      //             ),
+      //           ),
+      //           activeIcon: Padding(
+      //             padding: EdgeInsets.only(bottom: 8.0),
+      //             child: ImageIcon(
+      //               NetworkImage(
+      //                 homeIcon,
+      //               ),
+      //             ),
+      //           ),
+      //           label: 'Beranda',
+      //           tooltip: 'Home',
+      //         ),
+      //         BottomNavigationBarItem(
+      //           icon: Padding(
+      //             padding: EdgeInsets.only(bottom: 8.0),
+      //             child: ImageIcon(
+      //               NetworkImage(
+      //                 categoryIcon,
+      //               ),
+      //             ),
+      //           ),
+      //           activeIcon: Padding(
+      //             padding: EdgeInsets.only(bottom: 8.0),
+      //             child: ImageIcon(
+      //               NetworkImage(
+      //                 categoryIconActive,
+      //               ),
+      //             ),
+      //           ),
+      //           label: 'Kategori',
+      //           tooltip: 'Kategori',
+      //         ),
+      //         BottomNavigationBarItem(
+      //           icon: Padding(
+      //             padding: EdgeInsets.only(bottom: 8.0),
+      //             child: ImageIcon(
+      //               NetworkImage(
+      //                 shop,
+      //               ),
+      //             ),
+      //           ),
+      //           activeIcon: Padding(
+      //             padding: EdgeInsets.only(bottom: 8.0),
+      //             child: ImageIcon(
+      //               NetworkImage(
+      //                 shopActive,
+      //               ),
+      //             ),
+      //           ),
+      //           label: 'Toko',
+      //           tooltip: 'Toko',
+      //         ),
+      //         BottomNavigationBarItem(
+      //           icon: Padding(
+      //             padding: EdgeInsets.only(bottom: 8.0),
+      //             child: ImageIcon(
+      //               NetworkImage(
+      //                 feed,
+      //               ),
+      //             ),
+      //           ),
+      //           activeIcon: Padding(
+      //             padding: EdgeInsets.only(bottom: 8.0),
+      //             child: ImageIcon(
+      //               NetworkImage(
+      //                 feedActive,
+      //               ),
+      //             ),
+      //           ),
+      //           label: 'Feed',
+      //           tooltip: 'Feed',
+      //         ),
+      //         BottomNavigationBarItem(
+      //           icon: Padding(
+      //             padding: EdgeInsets.only(bottom: 8.0),
+      //             child: ImageIcon(
+      //               NetworkImage(
+      //                 profile,
+      //               ),
+      //             ),
+      //           ),
+      //           activeIcon: Padding(
+      //             padding: EdgeInsets.only(bottom: 8.0),
+      //             child: ImageIcon(
+      //               NetworkImage(
+      //                 profileActive,
+      //               ),
+      //             ),
+      //           ),
+      //           label: 'Profile',
+      //           tooltip: 'Profile',
+      //         ),
+      //       ],
+      //       currentIndex: controller.selectedIndex,
+      //       elevation: 0,
+      //       onTap: controller.onItemTapped,
+      //     ),
+      //   ),
+
+      // );
+      return PersistentTabView(
+        context,
+        controller: controller.navController,
+        screens: controller.page,
+        padding: const NavBarPadding.only(
+          left: 16,
         ),
+        margin: EdgeInsets.zero,
+        navBarHeight: 82,
+        
+        items: <PersistentBottomNavBarItem>[
+          PersistentBottomNavBarItem(
+            icon: const ImageIcon(
+              NetworkImage(
+                homeIcon,
+              ),
+            ),
+            inactiveIcon: const ImageIcon(
+              NetworkImage(
+                homeIconInactive,
+              ),
+            ),
+            title: "Beranda",
+            activeColorPrimary: primary,
+            inactiveColorPrimary: font,
+            textStyle: h5Medium,
+          ),
+          PersistentBottomNavBarItem(
+            icon: const ImageIcon(
+              NetworkImage(
+                categoryIconActive,
+              ),
+            ),
+            inactiveIcon: const ImageIcon(
+              NetworkImage(
+                categoryIcon,
+              ),
+            ),
+            title: "Kategori",
+            activeColorPrimary: primary,
+            inactiveColorPrimary: font,
+            textStyle: h5Medium,
+          ),
+          PersistentBottomNavBarItem(
+            icon: const ImageIcon(
+              NetworkImage(
+                shopActive,
+              ),
+            ),
+            inactiveIcon: const ImageIcon(
+              NetworkImage(
+                shop,
+              ),
+            ),
+            title: "Shop",
+            textStyle: h5Medium,
+            activeColorPrimary: primary,
+            inactiveColorPrimary: font,
+          ),
+          PersistentBottomNavBarItem(
+              icon: const ImageIcon(
+                NetworkImage(
+                  feedActive,
+                ),
+              ),
+              inactiveIcon: const ImageIcon(
+                NetworkImage(
+                  feed,
+                ),
+              ),
+              title: "Feed",
+              activeColorPrimary: primary,
+              inactiveColorPrimary: font,
+              textStyle: h5Medium),
+          PersistentBottomNavBarItem(
+              icon: const ImageIcon(
+                NetworkImage(
+                  profileActive,
+                ),
+              ),
+              inactiveIcon: const ImageIcon(
+                NetworkImage(
+                  profile,
+                ),
+              ),
+              title: "Shop",
+              activeColorPrimary: primary,
+              inactiveColorPrimary: font,
+              textStyle: h5Medium),
+        ],
+        confineInSafeArea: true,
+        backgroundColor: Colors.white, // Default is Colors.white.
+        handleAndroidBackButtonPress: true, // Default is true.
+        resizeToAvoidBottomInset:
+            true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+        stateManagement: true, // Default is true.
+        hideNavigationBarWhenKeyboardShows:
+            true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+        decoration: NavBarDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          colorBehindNavBar: Colors.white,
+        ),
+        popAllScreensOnTapOfSelectedTab: true,
+        popActionScreens: PopActionScreensType.all,
+        itemAnimationProperties: const ItemAnimationProperties(
+          // Navigation Bar's items animation properties.
+          duration: Duration(milliseconds: 200),
+          curve: Curves.ease,
+        ),
+        screenTransitionAnimation: const ScreenTransitionAnimation(
+          // Screen transition animation on change of selected tab.
+          animateTabTransition: true,
+          curve: Curves.ease,
+          duration: Duration(milliseconds: 200),
+        ),
+        navBarStyle:
+            NavBarStyle.style14, // Choose the nav bar style with this property.
       );
     });
   }
